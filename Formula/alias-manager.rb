cea3ac6395146d3d20473ce49789e50b2136ab2f
Hugo Sibony <hugo.sibony@epita.fr>
2025-11-18 20:42:34 +0100

    Update to v2.3.0

--- Formula/alias-manager.rb
+++ Formula/alias-manager.rb
@@ -1,8 +1,8 @@
 class AliasManager < Formula
     desc "Lightweight CLI tool to manage your shell aliases efficiently"
     homepage "https://github.com/kazetachinuu/alias_manager"
-    url "https://github.com/kazetachinuu/alias_manager/archive/v2.2.0.tar.gz"
-    sha256 "5cbc443adb31552d6d1b73c857c6d62be390aa9ee8b16d4c49aa4893f03fd374"
+    url "https://github.com/kazetachinuu/alias_manager/archive/v2.3.0.tar.gz"
+    sha256 "ff24e4f2fe14655c12b25b636eefcf757f4cccaf5080afc8af444402e55effc2"
     license "MIT"
 
     def install
